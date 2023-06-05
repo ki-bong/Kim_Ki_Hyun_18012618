c8d373c645a693f34c15e7cb61b8747afb22d484
win10 <kb980723@sju.ac.kr>
2023-06-06 04:24:16 +0900

    ECI2ECEF_DCM

---
+++ ECI2ECEF_DCM.m
@@ -0,0 +1,16 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Week#13 Homework
+%% 우주궤도역학(001)
+%% 18012618 김기현
+%% Coordinate Transform Functions(ECI to ECEF)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function [DCM] = ECI2ECEF_DCM(time)
+
+jd = juliandate(time);
+
+theta_g = siderealTime(jd);
+
+DCM = [cos(deg2rad(theta_g)) sin(deg2rad(theta_g)) 0;
+      -sin(deg2rad(theta_g)) cos(deg2rad(theta_g)) 0;
+      0 0 1];
